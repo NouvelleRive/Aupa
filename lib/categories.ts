94203f8d37a9e6c970adbee526d8bfecfa75923f
Salomé Kassabi <salomekassabi@MacBook-Air.local>
2026-04-05 22:10:30 +0200

    CategorieRecette derive de lib/categories

--- lib/categories.ts
+++ lib/categories.ts
@@ -3,4 +3,6 @@ export const CATEGORIES = [
   'Salade', 'Grignotage', 'Suppléments',
   'Le Chaud', 'Les Iced', 'Le Detox', 'Les Sodas', 'Les Eaux',
   'Les Binouz', 'Les Wines', 'Les Cocktailz', 'Les Apéritifs et Digestifs',
-];
+] as const;
+
+export type CategorieRecette = typeof CATEGORIES[number];
